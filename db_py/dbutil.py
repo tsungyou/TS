a570d38656a53fc5abc0bed68ae4c4f26289c579
Lee Jimmy <mini@Lees-Mac-mini.local>
2024-08-12 19:46:47 +0800

    delete files

--- db_py/dbutil.py
+++
@@ -1,8 +0,0 @@
-class dbutil(object):
-    '''
-    TWSE.py/get_TWSE_TWSE()
-    TPEX.py/get_TWOTCI_TPEX()
-    '''
-    def convert_to_2024(da):
-        year, month, day = da.split("/")
-        return f"{int(year)}-{month}-{day}"
