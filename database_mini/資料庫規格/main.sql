ae81039edd82750f32fb8a8601412e48f444e91e
Lee Jimmy <mini@Lees-Mac-mini.local>
2024-08-15 18:57:02 +0800

    update

---
+++ database_mini/資料庫規格/main.sql
@@ -0,0 +1,9 @@
+tw=# \dt
+              List of relations
+ Schema |        Name        | Type  | Owner 
+--------+--------------------+-------+-------
+ public | block_code3_deatil | table | mini
+ public | block_trade        | table | mini
+ public | maincode           | table | mini
+ public | price              | table | mini
+ public | user_credential    | table | mini
